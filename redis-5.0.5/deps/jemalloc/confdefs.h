d4e87126275f545ad738452c7f7577172621c9cd
Jonathan Luk <jluk87@gmail.com>
2019-08-22 17:58:20 +0000

    Automatically backed up by Learn

---
+++ redis-5.0.5/deps/jemalloc/confdefs.h
@@ -0,0 +1,8 @@
+/* confdefs.h */
+#define PACKAGE_NAME ""
+#define PACKAGE_TARNAME ""
+#define PACKAGE_VERSION ""
+#define PACKAGE_STRING ""
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
+#define JEMALLOC_HAS_RESTRICT 1
---
+++ redis-5.0.5/deps/jemalloc/conftest.c
@@ -0,0 +1,20 @@
+/* confdefs.h */
+#define PACKAGE_NAME ""
+#define PACKAGE_TARNAME ""
+#define PACKAGE_VERSION ""
+#define PACKAGE_STRING ""
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
+#define JEMALLOC_HAS_RESTRICT 1
+/* end confdefs.h.  */
+
+
+int
+main ()
+{
+
+    return 0;
+
+  ;
+  return 0;
+}
