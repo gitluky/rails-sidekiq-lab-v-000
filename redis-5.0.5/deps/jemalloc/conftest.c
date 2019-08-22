8d1f71f84b9c39249835bf2810f5d8417581df70
Jonathan Luk <jluk87@gmail.com>
2019-08-22 17:58:22 +0000

    Automatically backed up by Learn

--- redis-5.0.5/deps/jemalloc/conftest.c
+++ redis-5.0.5/deps/jemalloc/conftest.c
@@ -6,6 +6,39 @@
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_URL ""
 #define JEMALLOC_HAS_RESTRICT 1
+#define HAVE_CXX14 1
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define SIZEOF_VOID_P 8
+#define LG_SIZEOF_PTR 3
+#define SIZEOF_INT 4
+#define LG_SIZEOF_INT 2
+#define SIZEOF_LONG 8
+#define LG_SIZEOF_LONG 3
+#define SIZEOF_LONG_LONG 8
+#define LG_SIZEOF_LONG_LONG 3
+#define SIZEOF_INTMAX_T 8
+#define LG_SIZEOF_INTMAX_T 3
+#define HAVE_CPU_SPINWAIT 1
+#define CPU_SPINWAIT __asm__ volatile("pause")
+#define LG_VADDR 48
+#define LG_VADDR 48
+#define JEMALLOC_PURGE_MADVISE_DONTNEED_ZEROS  
+#define JEMALLOC_HAS_ALLOCA_H 1
+#define JEMALLOC_PROC_SYS_VM_OVERCOMMIT_MEMORY  
+#define JEMALLOC_THREADED_INIT  
+#define JEMALLOC_USE_CXX_THROW  
+#define HAVE_MALLOC_H 1
+#define JEMALLOC_USABLE_SIZE_CONST 
+#define JEMALLOC_HAVE_ATTR  
 /* end confdefs.h.  */
 
 
